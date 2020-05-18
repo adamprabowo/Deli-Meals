b752d6948165f4eb9fc340b0585112a9322f36e1
adampra28 <adamprabowo00@gmail.com>
2020-05-18 14:30:07 +0700

    Diving Deeper into Named Routes

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
@@ -9,10 +10,13 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).pushNamed('/category-meals', arguments: {
-      'id': id,
-      'title': title,
-    });
+    Navigator.of(ctx).pushNamed(
+      CategoryMealsScreen.routeName,
+      arguments: {
+        'id': id,
+        'title': title,
+      },
+    );
   }
 
   @override
